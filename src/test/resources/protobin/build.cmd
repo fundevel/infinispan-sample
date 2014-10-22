97bceed7019fb37dbd348a73686be10fe6700f36
ilhwan <seoilhwan@gmail.com>
2014-10-22 11:00:55 +0900

    protobin 파일들은 6.x에서 쓰던 것이므로 제거

--- src/test/resources/protobin/build.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-protoc.exe --descriptor_set_out=src/test/resources/protobin/mailbox.protobin src/test/resources/proto/mailbox.proto
-protoc.exe --descriptor_set_out=src/test/resources/protobin/player.protobin src/test/resources/proto/player.proto
